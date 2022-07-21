b7b99a250a196fc44d2ed5e1aed947d4561ccc88
Anderson de Aguiar de Oliveira <andersondeaguiardeoliveira@gmail.com>
2022-07-21 16:54:12 -0300

    2

--- principal/settings.py
+++ principal/settings.py
@@ -68,13 +68,13 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
-        'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', 'db'),
-        'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', ''),
-    },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'okteto',
+        'USER': 'okteto',
+        'PASSWORD':'okteto',
+        'HOST':'localhost',
+        'PORT': 5432
+    }
 }
 
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\sprint5\aplicacao-django-bot-ifrs\venv
+set VIRTUAL_ENV=C:\Users\ander\OneDrive\Desktop\aplicacao-django-bot-ifrs\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
